a918f397c379b905c0d220616215d989ef8bd872
Paul Ruvolo <paullundyruvolo@gmail.com>
2015-04-06 13:25:14 -0400

    added a script to download the smile dataset

---
+++ download_datasets.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget "http://occam.olin.edu/sites/default/files/load_smiles.py" -O ./datasets/load_smiles.py
+wget "http://occam.olin.edu/sites/default/files/smile_dataset.mat" -O ./datasets/smile_dataset.mat
